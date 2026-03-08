57c5ed266765a4cb49546cdc46749b55bbbed394
James Z. Zhang <james.z.zhang00@gmail.com>
2026-03-08 13:09:18 -0400

    Added zsh alias

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 My personal developed terminal tools for Bash/Zsh
 
 ### List
-1. `folder_descrition.sh` - allows to write a description for rarely used folders, the description will display in terminal when switching to the directory. The description can be removed by `rm .folder_description`
+1. `zsh_alias.sh` - some command shortcuts, including basic directory change, opening apps and folders
+2. `folder_descrition.sh` - allows to write a description for rarely used folders, the description will display in terminal when switching to the directory. The description can be removed by `rm .folder_description`
---
+++ zsh_alias.sh
@@ -0,0 +1,20 @@
+#####################################
+# Some shortened commands
+#####################################
+
+# 1. Basic operations
+alias cle="clear"
+alias o="open"
+
+# 2. Go back to the previous directory quicker
+alias cc="cd .."
+alias ccc="cd ../.."
+alias cccc="cd ../../.."
+
+# 3. Shorthand to open frequently used apps and folders
+alias vsc="open -a 'Visual Studio Code.app'" # Use the Mac OS built-in open command so it will always work without enabling the code command or update PATH
+alias vscn='code -n'
+alias vscr='code -r'
+alias cursor="open -a 'Cursor.app'"
+alias gc="open -a 'Google Chrome.app'"
+alias Desk="cd ~/Desktop"
